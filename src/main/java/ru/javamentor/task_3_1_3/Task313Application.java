da13fefc21e2c5f817daa842ee24ea72d6afd44f
Dmitriy <fed3375546@yandex.ru>
2020-09-02 14:01:15 +0300

    Initial commit

---
+++ src/main/java/ru/javamentor/task_3_1_3/Task313Application.java
@@ -0,0 +1,13 @@
+package ru.javamentor.task_3_1_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task313Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Task313Application.class, args);
+	}
+
+}
